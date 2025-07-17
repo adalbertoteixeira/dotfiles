ba820082b27f6ceed6004c0b2c0df4c7226efa08
Adalberto Teixeira <adalberto.teixeira@cdigital.pt>
2025-07-17 09:23:11 +0100

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -588,7 +588,7 @@ require("lazy").setup({
     },
     {
       "nvim-telescope/telescope.nvim",
-      tag = "0.1.x",
+      tag = "0.1.8",
       -- or                              , branch = '0.1.x',
       dependencies = { "nvim-lua/plenary.nvim" },
     },
