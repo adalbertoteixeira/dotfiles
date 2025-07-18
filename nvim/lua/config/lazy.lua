fdf83ac97ac1216fc63bfeffa927d3b62e01610a
Adalberto Teixeira <adalberto.teixeira@bengroup.com>
2025-07-18 18:52:04 +0100

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -59,7 +59,9 @@ require("lazy").setup({
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "default" },
+        keymap = {
+          preset = "default",
+        },
 
         appearance = {
           -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -68,7 +70,9 @@ require("lazy").setup({
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = {
+          documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -109,12 +113,14 @@ require("lazy").setup({
           http = { "kulala" },
           lua = { "stylua" },
           python = { "isort", "black" },
-          typescript = { "biome", "lua-language-server", "eslint" },
-          javascript = { "biome", "biome-organize-imports", "eslint" },
-          typescriptreact = { "biome", "biome-organize-imports", "eslint" },
+          typescript = { "biome", "biome-organize-imports", "eslint_d" },
+          typescriptreact = { "biome", "biome-organize-imports", "eslint_d" },
+          javascript = { "biome", "biome-organize-imports", "eslint_d" },
+          javascriptreact = { "biome", "biome-organize-imports", "eslint_d" },
           json = { "biome" },
           svelte = { "biome", "biome-organize-imports" },
           hcl = { "packer_fmt" },
+          sql = { "sqruff" },
           -- terraform = { "terraform_fmt" },
           -- tf = { "terraform_fmt" },
           -- -- rust = { "rust_analyser", lsp_format = "fallback" },
@@ -334,8 +340,10 @@ require("lazy").setup({
       opts = {
         ensure_installed = {
           "lua_ls",
+          "kulala_ls",
           "biome",
           "shellcheck",
+          "js-debug-adapter",
           --"rust_analyzer",
           -- "codelldb"
           -- "bacon",
@@ -374,10 +382,153 @@ require("lazy").setup({
           "stylelint",
           "tflint",
           "typos-cli",
+          "kulala_ls",
           -- "bacon",
         },
       },
     },
+    {
+      "mfussenegger/nvim-dap",
+      recommended = true,
+      desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
+      dependencies = {
+        "rcarriga/nvim-dap-ui",
+        -- virtual text for the debugger
+        {
+          "theHamsta/nvim-dap-virtual-text",
+          opts = {},
+        },
+      },
+
+      configuration = {
+        javascript = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+        },
+      },
+      -- stylua: ignore
+      keys = {
+	{ "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+	{ "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+	{ "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
+	{ "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+	{ "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+	{ "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+	{ "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+	{ "<leader>dj", function() require("dap").down() end, desc = "Down" },
+	{ "<leader>dk", function() require("dap").up() end, desc = "Up" },
+	{ "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+	{ "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+	{ "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+	{ "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
+	{ "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+	{ "<leader>ds", function() require("dap").session() end, desc = "Session" },
+	{ "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+	{ "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      },
+
+      config = function()
+        -- load mason-nvim-dap here, after all adapters have been setup
+        require("mason-nvim-dap").setup({
+          -- Makes a best effort to setup the various debuggers with
+          -- reasonable debug configurations
+          automatic_installation = true,
+
+          -- You can provide additional configuration to the handlers,
+          -- see mason-nvim-dap README for more information
+          handlers = {},
+
+          -- You'll need to check that you have the required things installed
+          -- online, please don't ask me how to install them :)
+          ensure_installed = {
+            "js-debug-adapter",
+            -- Update this to ensure that you have the debuggers for the langs you want
+          },
+        })
+
+        vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+        -- for name, sign in pairs(LazyVim.config.icons.dap) do
+        --   sign = type(sign) == "table" and sign or { sign }
+        --   vim.fn.sign_define("Dap" .. name, {
+        --     text = sign[1],
+        --     texthl = sign[2] or "DiagnosticInfo",
+        --     linehl = sign[3],
+        --     numhl = sign[3],
+        --   })
+        -- end
+        --
+        -- -- setup dap config by VsCode launch.json file
+        -- local vscode = require("dap.ext.vscode")
+        -- local json = require("plenary.json")
+        -- vscode.json_decode = function(str)
+        --   return vim.json.decode(json.json_strip_comments(str))
+        -- end
+      end,
+    },
+    {
+      "rcarriga/nvim-dap-ui",
+      -- virtual text for the debugger
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+    },
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      dependencies = "mason.nvim",
+      cmd = { "DapInstall", "DapUninstall" },
+      opts = {
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_installation = true,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
+
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          "js-debug-adapter",
+          "vscode-js-debug",
+          -- Update this to ensure that you have the debuggers for the langs you want
+        },
+      },
+      -- mason-nvim-dap is loaded when nvim-dap loads
+      config = function() end,
+    },
+    {
+      "rcarriga/nvim-dap-ui",
+      dependencies = { "nvim-neotest/nvim-nio" },
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+      opts = {},
+      config = function(_, opts)
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          dapui.close({})
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          dapui.close({})
+        end
+      end,
+    },
+    { "nvim-neotest/nvim-nio" },
     -- {
     -- 	"mrcjkb/rustaceanvim",
     -- 	version = "^6", -- Recommended
@@ -791,21 +942,60 @@ require("gitsigns").setup({
   },
 })
 
--- vim.lsp.config("rust_analyzer", {
--- 	-- settings = {
--- 	["rust-analyzer"] = {
--- 		-- enabled = false,
--- 		diagnostics = {
--- 			enable = false,
--- 		},
--- 		checkOnSave = {
--- 			enable = false,
--- 		},
--- 	},
--- 	-- },
--- })
+require("dap").adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    -- 💀 Make sure to update this path to point to your installation
+    args = { os.getenv("HOME") .. "/.js-debug/src/dapDebugServer.js", "${port}" },
+  },
+}
+
+require("dap").configurations.typescript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach to Node app",
+    address = "localhost",
+    port = 9229,
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+  },
+}
+require("dap").configurations.javascript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach to Node app",
+    address = "localhost",
+    port = 9229,
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+  },
+}
+
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("kulala_ls")
+vim.lsp.enable("postgres_lsp")
+vim.lsp.config("lua_ls", {})
 -- vim.lsp.config("bacon_ls", {
 -- 	settings = {
 -- 		-- enabled = diagnostics == "bacon-ls",
